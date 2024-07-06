1c4002ebbb61554001803894cefd85e85a012914
Jordan W. Mungo <jwmungo@gmail.com>
2024-07-06 01:20:03 -0400

    Separate folders

---
+++ ScriptTests/selenium_test_1.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from ..Pages.PerspectivePageObject import PerspectivePageObject
+
+
+with webdriver.Chrome() as web_driver:
+    page = PerspectivePageObject(web_driver, 'http://localhost:8088', '/Testing')
